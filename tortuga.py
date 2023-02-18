a3de0050d8e3695f4e88d88397cb41968772280e
Camilo <castillo.jcamilo10@gmail.com>
2023-02-18 00:32:43 -0500

    optimizando codigo 1.0

--- tortuga.py
+++ tortuga.py
@@ -1,13 +1,17 @@
-import turtle
+import turtle # IMPORATA LA LIBRERIA
 
+# CREA VENTANA DE VISUALZACION GRAFICA
 ventana = turtle.Screen()
 
+# CREA EL OBJETO, LA CUAL SERA LA "TORTUGA" QUE DIBUJARA EL GRAFICO
 tortuga = turtle.Turtle()
 
+# BUCLE REPITIENDO 4 VECES UNA LINEA DE 100 PX GIRANDO HACIA LA DERECHA EN CADA REPETICION
 for i in range(4):
     tortuga.forward(100)
     tortuga.right(90)
     
+# ABRE LA VENTANA DONDE MOSTRARA EL GRAFICO AUTOMATICAMENTE
 ventana.exitonclick()
 
 '''window = turtle.Screen()
